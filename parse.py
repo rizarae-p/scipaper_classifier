0684dcd5cebdf0bc31c1a1a001b856dbf5378cb8
anna-teruel <teruel.anna@gmail.com>
2023-09-22 10:35:29 +0200

    made some changes in count_keywords function

--- parse.py
+++ parse.py
@@ -47,36 +47,44 @@ def tokenize_and_match(text):
 	return matched_texts
 
 def count_keywords(file_name='madlc_citations.ris'):
-	"""
-	Count occurences of keywords in the titles  and abstracts of RIS entries. 
+    """
+    Count occurrences of keywords in the titles and abstracts of RIS entries.
 
-	Args:
-		file_name (str, optional): the name of RIS file to process. 
+    Args:
+        file_name (str, optional): The name of the RIS file to process.
 
-	Returns:
-		dict: A dictionary where keys are keywords and values are the number of times each keyword appears
-              in either the titles or abstracts of the RIS entries.
-	"""	
-	entries = []
-	with open(file_name, 'r', encoding='utf-8') as file:
-	    entries = list(rispy.load(file))
+    Returns:
+        dict: A dictionary where keys are keywords, and values are dictionaries with counts and lists of titles.
+    """    
+    entries = []
+    with open(file_name, 'r', encoding='utf-8') as file:
+        entries = list(rispy.load(file))
 
-	entries = [dict(i) for i in entries]
-	keys = {}
-	for entry in entries:
-		title = clean_html(entry['title'])
-		abstract = clean_html(entry['abstract'])
-		keywords = Counter(set(tokenize_and_match(title)))
-		if len(keywords) < 1:
-			keywords = Counter(tokenize_and_match(abstract))
-			if len(keywords) < 1:
-				continue
-		top_keyword = sorted(keywords, key=lambda x: (-keywords[x], x))[0]
-		if top_keyword not in keys.keys():
-			keys[top_keyword] = 1
-		else:
-			keys[top_keyword] +=1
-	return keys
+    entries = [dict(i) for i in entries]
+    keys = {}
+    
+    for entry in entries:
+        title = clean_html(entry['title'])
+        abstract = clean_html(entry['abstract'])
+        keywords = Counter(set(tokenize_and_match(title)))
+        
+        if len(keywords) < 1:
+            keywords = Counter(tokenize_and_match(abstract))
+            if len(keywords) < 1:
+                top_keyword = 'Others'
+            else:
+                top_keyword = sorted(keywords, key=lambda x: (-keywords[x], x))[0]
+        else:
+            top_keyword = sorted(keywords, key=lambda x: (-keywords[x], x))[0]
 
-
+        if top_keyword not in keys:
+            keys[top_keyword] = {'count': 1, 'titles': [entry['title']]}
+        else:
+            keys[top_keyword]['count'] += 1
+            keys[top_keyword]['titles'].append(entry['title'])
+            
+    total_entries = len(entries)  # Count the total number of entries
+    print(f"Total number of entries in the RIS file: {total_entries}")
+    
+    return keys
 
